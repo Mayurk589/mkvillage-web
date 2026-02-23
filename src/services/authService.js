b3d725125f42089c1db8cd4ccf711823533bfa9e
Mayuresh Kumbar <115691851+Mayurk589@users.noreply.github.com>
2026-02-23 19:35:36 +0530

    Update authService to use HTTPS for API calls

--- src/services/authService.js
+++ src/services/authService.js
@@ -14,6 +14,6 @@ export const getUserByMobile = (mobile) =>
   api.get(
     `/internal/users/by-mobile/${encodeURIComponent(mobile)}`,
     {
-      baseURL: "http://mkvillage-user-service-env.eba-hkcstuwr.us-east-1.elasticbeanstalk.com"  // remove /api
+      baseURL: "https://mkvillage-user-service-env.eba-hkcstuwr.us-east-1.elasticbeanstalk.com"  // remove /api
     }
   );
